a1f623397752a77864126464bc3f805e53a7756a
NekaNeka Neka <NekaNeka Neka>
2016-07-19 00:41:38 +0700

    Finished webpack build

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,15 +2,22 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-	entry: '.app/containers/index.js',
+	entry: {
+		js: './app/index.js',
+		html: './app/index.html'
+	},
 	output: {
 		path: './build/',
 		filename: 'js/bundle.js'
 	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
 	devtool: 'eval',
 	module: {
 		loaders: [
-			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+			{ test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
+			{test: /index\.html$/, loader: 'file-loader', query: {name: 'index.html'}}
 		]
 	}
 };
